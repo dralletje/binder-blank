3c779a11f5d5ea0632fd106a8772e4149ebfb7e0
Michiel Dral <m.c.dral@gmail.com>
2020-09-14 21:34:57 +0200

    ffffff

--- plutoserver.py
+++ plutoserver.py
@@ -1,6 +1,6 @@
 def setup_plutoserver():
   return {
-    "command": ["julia", "-Jprecompile.so", "-e", 'import Pluto; Pluto.run("0.0.0.0", {port}; configuration=Pluto.ServerConfiguration(launch_browser=false), security=Pluto.ServerSecurity(false))'],
+    "command": ["/usr/local/julia/bin/julia", "-J/home/pluto/precompile.so", "-e", 'import Pluto; Pluto.run("0.0.0.0", {port}; configuration=Pluto.ServerConfiguration(launch_browser=false), security=Pluto.ServerSecurity(false))'],
     "timeout": 60,
     "launcher_entry": {
         "title": "Pluto.jl",
